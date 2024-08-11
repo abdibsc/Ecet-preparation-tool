25854f14eca98c8787a3cebd60093d2b626ec0c6
godikemahesh <69629676+godikemahesh@users.noreply.github.com>
2024-08-12 00:13:50 +0530

    Update gpt_data.py

--- manage/gpt_data.py
+++ manage/gpt_data.py
@@ -1,15 +1,6 @@
 from openai import OpenAI
 import sys
-import spacy
 
-nlp=spacy.load("en_core_web_sm")
-def lemitization(lst):
-    lemed=[]
-    for i in range(len(lst)):
-        doc=nlp(lst[i])
-        processed_text = " ".join([token.lemma_ for token in doc if not token.is_stop])
-        lemed.append(processed_text)
-    return lemed
 def get(state):
     client = OpenAI(
         api_key="211fa21c08be45e86847eff9d61cb4e6bc7374995f3b7ac894d34f2395a415dd",
