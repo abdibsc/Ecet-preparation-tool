86e73bd3bda67f16326c30dc527c7411e8d29f51
godikemahesh <69629676+godikemahesh@users.noreply.github.com>
2024-08-13 20:21:44 +0530

    Update main.py

--- manage/main.py
+++ manage/main.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from mapps import electronics,cse,civil,home,mech,eee
 from streamlit_option_menu import option_menu
+import road_map
 st.set_page_config(page_title="ecet prep tool")
 st.title("SuRaj ECET preparation Tool")
 class MultiApp:
@@ -13,24 +14,31 @@ class MultiApp:
         })
     def run(self):
         with st.sidebar:
-            app=option_menu(
-                menu_title="choose your Bracnch",
-                options=["Home","Electronics","Computers","Civil","Mech","EEE"],
-                icons=["house-fill"],
-                menu_icon="chat-text-fill",
-                default_index=0
-            )
-            st.write("Note:Please click 2 times to take right action")
-        if app=="Home":
-            home.app()
-        if app=="Electronics":
-            electronics.app()
-        if app=="Computers":
-            cse.app()
-        if app=="Civil":
-            civil.app()
-        if app=="Mech":
-            mech.app()
-        if app=="EEE":
-            eee.app()
+            road=option_menu(menu_title="",options=["Choose Branch","Study Road Map"])
+            if road=="Choose Branch":
+                app=option_menu(
+                    menu_title="choose your Bracnch",
+                    options=["Home","Electronics","Computers","Civil","Mech","EEE"],
+                    icons=["house-fill"],
+                    menu_icon="chat-text-fill",
+                    default_index=0
+                )
+
+                st.write("Note:Please click 2 times to take right action")
+        if road=="Choose Branch":
+            if app=="Home":
+                home.app()
+            if app=="Electronics":
+                electronics.app()
+            if app=="Computers":
+                cse.app()
+            if app=="Civil":
+                civil.app()
+            if app=="Mech":
+                mech.app()
+            if app=="EEE":
+                eee.app()
+        if road=="Study Road Map":
+            road_map.rmap()
+
     run(self=None)
