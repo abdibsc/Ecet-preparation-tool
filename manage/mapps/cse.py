2657c3857849674fd4a3fe289fe13892b9ec2dc8
godikemahesh <69629676+godikemahesh@users.noreply.github.com>
2024-08-13 23:43:34 +0530

    Update cse.py

--- manage/mapps/cse.py
+++ manage/mapps/cse.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
 from subjects import chemistry, physics,maths,computers
-
+from mapps import papers
 
 def app():
     # Check if a subject is already selected
@@ -12,7 +12,7 @@ def app():
     if st.session_state.selected is None:
         sub = option_menu(
             menu_title="Choose Subject here",
-            options=["select","Chemistry", "Physics", "Maths", "Computers"],
+            options=["select","Chemistry", "Physics", "Maths", "Computers","Previous Papers"],
             menu_icon="book-fill",
             default_index=0
         )
@@ -29,7 +29,8 @@ def app():
         maths.app()
     elif st.session_state.selected == "Computers":
         computers.app()
-
+    elif st.session_state.selected=="Previous Papers":
+        papers.cse()        
     # Add a reset button to clear the selection and show the menu again
     if st.session_state.selected is not None:
         if st.button("Choose Another Subject"):
