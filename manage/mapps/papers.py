b230adb98208d4efebbd68fd95f8fa96d0c4126d
godikemahesh <69629676+godikemahesh@users.noreply.github.com>
2024-08-13 23:40:14 +0530

    Add files via upload

---
+++ manage/mapps/papers.py
@@ -0,0 +1,38 @@
+import streamlit as st
+def ece():
+    st.subheader("Electronics Previous Question Papers")
+    st.write(f"2023 Question paper: [Download paper](https://media.collegedekho.com/media/uploads/2024/03/11/ts-ecet-2023-ece.pdf)")
+    st.write("2022 Question paper: [Download paper](https://media.collegedekho.com/media/uploads/2024/03/11/electronics-and-communication-engineering.pdf)")
+    st.write("2021 Question paper:[Download paper](https://media.collegedekho.com/media/uploads/2024/03/11/ece.pdf)")
+    st.write("2020 Question paper: [Download paper](https://media.collegedekho.com/media/uploads/2024/03/11/ece-1.pdf)")
+    st.write("2019 Question paper: [Download paper](https://media.collegedekho.com/media/uploads/2024/03/11/electronics-and-communication-engineering-1.pdf)")
+
+
+def cse():
+    st.subheader("CSE Previous Question Papers")
+    st.write("2023 Question paper: [Download paper](https://media.collegedekho.com/media/uploads/2024/03/11/ts-ecet-2023-cse.pdf)")
+    st.write("2023 Question paper: [Download paper](https://media.collegedekho.com/media/uploads/2024/03/11/computer-science-and-engineering_dI8PkeQ.pdf)")
+    st.write("2021 Question paper: [Download paper](https://media.collegedekho.com/media/uploads/2024/03/11/cse.pdf)")
+    st.write("2020 Question paper: [Download paper](https://media.collegedekho.com/media/uploads/2024/03/11/cse-1.pdf)")
+    st.write("2019 Question paper: [Download paper](https://media.collegedekho.com/media/uploads/2024/03/11/computer-science-and-engineering-paper.pdf)")
+def eee():
+    st.subheader("EEE Previous Question Papers")
+    st.write("2023 Question paper: [Download paper](https://media.collegedekho.com/media/uploads/2024/03/11/ts-ecet-2023-electrical-and-electronics.pdf)")
+    st.write("2022 Question paper: [Download paper](https://media.collegedekho.com/media/uploads/2024/03/11/electrical-and-electronics-engineering.pdf)")
+    st.write("2021 Question paper: [Download paper](https://media.collegedekho.com/media/uploads/2024/03/11/eee.pdf)")
+    st.write("2020 Question paper: [Download paper](https://media.collegedekho.com/media/uploads/2024/03/11/eee-2.pdf)")
+    st.write("2019 Question paper: [Download paper](https://media.collegedekho.com/media/uploads/2024/03/11/electrical-and-electronics-engineering-1.pdf)")
+def mech():
+    st.subheader("MECH Previous Question papers")
+    st.write("2023 Question paper: [Download paper](https://media.collegedekho.com/media/uploads/2024/03/11/ts-ecet-2023-mechanical.pdf)")
+    st.write("2022 Question paper: [Download paper](https://media.collegedekho.com/media/uploads/2024/03/11/mechanical-engineering.pdf)")
+    st.write("2021 Question paper: [Download paper](https://media.collegedekho.com/media/uploads/2024/03/11/mech.pdf)")
+    st.write("2020 Question paper: [Download paper](https://media.collegedekho.com/media/uploads/2024/03/11/mech-1.pdf)")
+    st.write("2019 Question paper: [Download paper](https://media.collegedekho.com/media/uploads/2024/03/11/mechanical-engineering-1.pdf)")
+def civil():
+    st.subheader("Civil Previous Question Papers")
+    st.write("2023 Question paper: [Download paper](https://media.collegedekho.com/media/uploads/2024/03/11/ts-ecet-2023-civil.pdf)")
+    st.write("2022 Question paper: [Download paper](https://media.collegedekho.com/media/uploads/2024/03/11/civil-engineering.pdf)")
+    st.write("2021 Question paper: [Download paper](https://media.collegedekho.com/media/uploads/2024/03/11/civil.pdf)")
+    st.write("2020 Question paper: [Download paper](https://media.collegedekho.com/media/uploads/2024/03/11/civil-1.pdf)")
+    st.write("2019 Question paper: [Download paper](https://media.collegedekho.com/media/uploads/2024/03/11/civil-engineering-1.pdf)")
