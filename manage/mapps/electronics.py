e2b36fd07c0869c3cb4c9674bd5471e2e97c3013
godikemahesh <69629676+godikemahesh@users.noreply.github.com>
2024-08-13 23:46:31 +0530

    Update electronics.py

--- manage/mapps/electronics.py
+++ manage/mapps/electronics.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
 from subjects import chemistry, physics,maths,electronics_sub
-
+from mapps import papers
 
 def app():
 
@@ -13,7 +13,7 @@ def app():
     if st.session_state.selected_subject is None:
         sub = option_menu(
             menu_title="Choose Subject here",
-            options=["select","Chemistry", "Physics", "Maths", "Electronics"],
+            options=["select","Chemistry", "Physics", "Maths", "Electronics","Previous Papers"],
             menu_icon="book-fill",
             default_index=0
         )
@@ -30,7 +30,8 @@ def app():
         maths.app()
     elif st.session_state.selected_subject == "Electronics":
         electronics_sub.app()
-
+    elif st.session_state.selected_subject=="Previous Papers":
+        papers.ece()
     # Add a reset button to clear the selection and show the menu again
     if st.session_state.selected_subject is not None:
         if st.button("Choose Another Subject"):
