1c3c4436bb245b34ef583206d8d31be505631b76
godikemahesh <69629676+godikemahesh@users.noreply.github.com>
2024-08-13 23:44:43 +0530

    Update eee.py

--- manage/mapps/eee.py
+++ manage/mapps/eee.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
 from subjects import chemistry, physics,maths,eee_sub
-
+from mapps import papers
 
 def app():
     # Check if a subject is already selected
@@ -12,7 +12,7 @@ def app():
     if st.session_state.eee is None:
         sub = option_menu(
             menu_title="Choose Subject here",
-            options=["select","Chemistry", "Physics", "Maths", "Electrical"],
+            options=["select","Chemistry", "Physics", "Maths", "Electrical","Previous Papers"],
             menu_icon="book-fill",
             default_index=0
         )
@@ -29,7 +29,8 @@ def app():
         maths.app()
     elif (st.session_state.eee== "Electrical"):
         eee_sub.app()
-
+    elif st.session_state.eee=="Previous Papers":
+        papers.eee()
     # Add a reset button to clear the selection and show the menu again
     if st.session_state.eee is not None:
         if st.button("Choose Another Subject"):
